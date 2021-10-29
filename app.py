b3126e723c1415d4d2833e02e3a56d4b9b338afe
Nepmia <yukana.miasu@gmail.com>
2021-10-29 15:58:37 +0200

    Refactored app and finished the making of the basic navigation system, tho article page is in wip

--- app.py
+++ app.py
@@ -13,38 +13,14 @@ app = Flask(__name__)
 
 article_data=[first_week, second_week,third_week,fourth_week,fifth_week]
 
-
-@app.route("/")
-def hello_world():
-    return render_template("test.html",  idiot="clément est PD")
-
 @app.route("/nos-articles")
 def article_theme_lister():
-
     return render_template("article_lister.html", article_data=article_data )
 
 @app.route("/nos-articles/<theme>")
 def article_theme_index(theme):
-
     for item in article_data:
         if theme in item["name"]:
-            match = "yes!"
-            print(theme)
-            print(item["name"])
-            print("pog")
-            return render_template("article_theme_index.html", article_data=article_data, current_page=theme, match=match)
-        else:
-            match = "no :c"
-            print(theme)
-            print(item["name"])
-            print("sadpog")
-    return render_template("article_theme_index.html", article_data=article_data, current_page=theme, match=match)
-
-@app.route("/articles")
-def articles_title_lister():
+            return render_template("article_theme_index.html", article_data=item, current_page=theme)
+    return render_template("article_theme_index.html", article_data=article_data, current_page=theme)
     
-    return render_template("article_lister.html",  json_data=first_week)
-
-
-
-
