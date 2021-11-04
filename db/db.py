bc2d69c820cee243df7f44571a402019c5597c1c
Nepmia <yukana.miasu@gmail.com>
2021-11-04 15:13:52 +0100

    Removed useless vars in create_app to replace them with env_var

--- db/db.py
+++ db/db.py
@@ -9,8 +9,8 @@ db = SQLAlchemy()
 def create_app():
     app = Flask(__name__)
 
-    app.config['SECRET_KEY'] = 'secret-key-goes-here'
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+    app.config['SECRET_KEY'] = SECRET_KEY
+    app.config['SQLALCHEMY_DATABASE_URI'] = DB_SOURCE
 
     db.init_app(app)
 
