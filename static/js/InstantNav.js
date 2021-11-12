7cd21df5d562d6b9d926fef4aa626e38d6219740
Nepmia <yukana.miasu@gmail.com>
2021-11-12 13:42:51 +0100

    Made the content loading animation, added a back btn to article pages but I have a strange issue with the back history btn not returning correct locations, will fix that in next commit

--- static/js/InstantNav.js
+++ static/js/InstantNav.js
@@ -1,23 +1,61 @@
 var pageLoadingInstance = false;
-var previous_page = null;
-var previous_title = null;
+var kekInstance = false;
+var url_data = {
+    previous_page : null,
+    previous_title : null,
+    current_page : null,
+    current_title:  null,
+}
+
+$(document).ready( () => {
+    $(".content").removeClass("c-off");
+    if (url_data.previous_page == null) {
+        $(".returnBtn").remove();
+    }
+    url_data.current_page = document.documentURI;
+    url_data.current_title = document.title;
+})
 
 window.onpopstate = () => {
-    var previous_title = document.title;
-    var previous_page = window.location.pathname;
-    changePage(previous_page, previous_title)
+    changePage(url_data.previous_page, url_data.previous_title)
 }
 
 $(document).on("click", ".pageChanger", function(){
     let newUrl = encodeURI($(this).attr("page"));
     let newTitle = $(this).attr("title") + " - StreetZine";
     changePage(newUrl, newTitle)
-    window.history.pushState(newUrl, newTitle, newUrl);
+    window.history.pushState(newUrl, url_data.previous_title, url_data.current_page);
 })
 
-function changePage (page, title) {
+$(document).on("click", ".returnBtn", function() {
+    changePage(url_data.previous_page, url_data.previous_title)
+    window.history.pushState(url_data.current_page, url_data.previous_title, url_data.current_page);
+})
 
-    $(".content").load(page + " .content")
+function changePage (page = null, title = null) {
+    url_data.current_page = page;
+    url_data.current_title = title;
+    setPreviousUrlData();
+    clearTimeout(pageLoadingInstance);
     document.title = title;
+    $(".content").addClass("c-off-alternate");
+    pageLoadingInstance = setTimeout( () => {
+        $(".content").empty();
+        $(".content").removeClass("c-off-alternate");
+        $(".content").load(page + " .content");
+        setTimeout( () => {
+            $(".content").removeClass("c-off");
+        },50)
+    }, 300)
+    kekInstance = setTimeout(() => {
+        if ($(".content").hasClass("c-off")) {
+            $(".content").removeClass("c-off");
+        }
+    },500)
+}
 
+function setPreviousUrlData() {
+    url_data.previous_title = document.title;
+    url_data.previous_page = document.documentURI;
+    console.log(url_data);
 }
