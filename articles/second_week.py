82152b2f667d0f810ef8dcc627567599b496b703
Mitsiky <90917286+Mitsiky7@users.noreply.github.com>
2021-11-19 10:58:05 +0100

    change date

--- articles/second_week.py
+++ articles/second_week.py
@@ -4,7 +4,7 @@ second_week = {
             "main": 1,
             "title": "Les collages anti-féminicide",
             "author": "Apolline",
-            "date": "18.10.2021",
+            "date": "18/10/2021",
             "content": {
                 "text": "Sûrement vous promener dans la rue ces derniers temps vous a permis de trouver les messages impactant des colleuses. Elles collent des phrases en papier lettre par lettre dénonçant féminicides, viols, et toutes sortes de violences à l'égard des femmes. <br><br>Un regard dur, Lola justifie ça par le fait que l’on lui à appris à ne pas sourire sur les images qu’elle renvoie, toujours être en colère, elles défendent un sujet grave qui nécessite d’être prises au sérieux, en générale la culture d’aujourd’hui renvoie une image très sexualisé des femmes, il faut casser cette idée et inspirer les femmes.",
                 "image1": "/static/img/colleuse.jpg",
@@ -15,7 +15,7 @@ second_week = {
             "main": 0,
             "title": "Rendre hommage",
             "author": "Callista",
-            "date": "19.10.2021",
+            "date": "19/10/2021",
             "content": {
                 "text": "Le street art, ce n’est pas seulement embellir les murs ou dénoncer certaines injustices. Certains artistes en profitent aussi pour rendre hommage à diverses personnalités, parfois à des institutions entières. <br><br>Par exemple, le 5 mars 2021, le street artiste JR a rendu hommage au cinéma, réalisant deux gigantesques collages reprenant une image mythique de Charlie Chaplin dans The Kid (1921) et de Harry Lloyd dans Monte là-dessus (1923). Réalisé pendant le confinement, il évoquait sa lassitude concernant la fermeture des cinémas. <br><br>Mais JR n’est pas le seul à avoir fait cela. Tout autour du monde, on a vu des oeuvres rendant hommage aux attentats de Paris ou encore à George Floyd. Que ce soit en France, aux États-Unis, en Espagne, en Angleterre, en Syrie ou encore en Irlande, de nombreux artistes ont décidé de lui rendre hommage. <br><br>Récemment, Jef Aérosol a fait d’un char Sherman de la Seconde Guerre mondiale une œuvre d’art en rendant hommage aux victimes de la guerre. Il a appelé cette œuvre War is Hell. Elle représente des victimes de victimes, mais on retrouve aussi quelques mots inscrits sur le char. <br><br>Ainsi, le street art ne permet pas seulement d’embellir et ne s’en tient pas seulement aux murs mais se diversifie davantage en utilisant d’autres médiums et d’autres fonctions.",
                 "image1": "/static/img/identité.png",
@@ -25,7 +25,7 @@ second_week = {
             "main": 0,
             "title": "Boulevard Paris 13, le musée en plein air",
             "author": "Louise",
-            "date": "20.10.2021",
+            "date": "20/10/2021",
             "content": {
                 "text": "Si vous vous êtes déjà baladé dans le 13e arrondissement de Paris ou que vous avez pris la ligne 6, entre quai de la gare et place d’Italie, vous avez sûrement dû voir des grandes fresques sur des bâtiments. C’est le Boulevard Paris 13, un musée sur le street art en plein air. <br><br>Autrefois le 13e arrondissement était gris, le quartier était peu flatteur. Elle s’est complètement métamorphosée et met ses tours en valeur grâce aux street artistes du monde entier. Le but est de soigner l’image du quartier et d'attirer les jeunes.<br>Depuis 2009, le 13e arrondissement propose aux Parisiens et aux touristes un parcours de fresques réalisées par des artistes français et internationaux. Initiée par la Galerie Itinérance, en partenariat avec la mairie du 13e arrondissement, cette balade a été pensée comme un véritable musée à ciel ouvert. En offrant un lieu d’expression à des artistes majeurs de l’art urbain, cet arrondissement écrit progressivement l’une des plus belles pages de l’histoire de Street Art et office au quartier une renommée internationale.<br><br>Boulevard Paris 13 possède un site, sur lequel vous pouvez retrouver les différentes œuvres, les artistes ainsi que le parcours. Vous y retrouverez des artistes tels que C215, David de la Mano, Invader et bien d’autres.<br>Si vous passez dans le 13e arrondissement, alors n’hésitez pas à faire le parcours !",
                 "image1": "https://pbs.twimg.com/media/EvuM4aaWgAQOQ7Z.jpg",
@@ -35,7 +35,7 @@ second_week = {
             "main": 0,
             "title": "Le street art et la pub",
             "author": "Clément",
-            "date": "21.10.2021",
+            "date": "21/10/2021",
             "content": {
                 "text": "Le street art source de liberté et d'expression permet à l’exprimant de montrer son point de vue. La pub ne sera pas épargnée par cet art, ce qui permettra à certaines marques de chaussures par exemple de promouvoir et vendre leurs produits (Louboutin en 2012 & Nike). Le street art ne sera pas le seul à influencer la pub, car la pub elle aussi influence le street art aussi bien positivement que négativement, avec l’apparition de certains artistes comme Jordan Seiler, Thom Thom, Vermibus et Ox qui décideront d’arranger les pubs à leur manière pour montrer leur point de vue anti-pub. <br>Jordan Seiler lutte dans le monde entier pour contrer la pollution visuelle et la pression publicitaire, grâce à ces œuvres à la fois minimalistes et très graphiques.<br>Thom Thom, armé de son cutter, entaille et mutile les affiches avec une habile précision. Que ce soit directement sur le panneau publicitaire ou sur des affiches arrachées, le style tranchant et incisif de Thom Thom ne laisse pas indifférent.",
                 "image1": "/static/img/pub1.png",
@@ -46,7 +46,7 @@ second_week = {
             "main": 0,
             "title": "Revendiquer une identité",
             "author": "Callista",
-            "date": "22.10.2021",
+            "date": "22/10/2021",
             "content": {
                 "text": "Le street art possède une spécificité particulière : celle d’être exposé aux yeux de tous, dans un espace public. Il suffit donc de seulement marcher dans la rue pour apercevoir l'œuvre d’un street artiste. Cet art est souvent utilisé pour embellir, dénoncer, mais aussi pour revendiquer une identité. Parfois, l’artiste ne revendique que son identité propre, mais ils sont aussi le reflet d’une culture entière. Il ne cherche pas simplement à revendiquer une identité, mais à montrer un sentiment d’appartenance à un groupe d’individus qui reflète la personne que l’on est. <br>Cette part d’appartenance peut aller de l’appartenance au groupe des street artistes comme à d’autres communautés qui n’ont pas forcément un lien avec l’art. <br><br>C’est ainsi que l’artiste raconte son histoire. Il permettra aussi à d’autres de s’identifier à leur tour. C’est aussi à ce moment-là que l’on constate que la ligne entre la revendication d’une identité, la dénonciation et l'embellissement est fine. Car chaque fois que l’on revendique son identité, on est susceptible de montrer un sentiment d’appartenance tout en dénonçant des actes contre ce groupe ou tout en embellissant un simple mur. ",
                 "image1": "/static/img/floyd.png",
