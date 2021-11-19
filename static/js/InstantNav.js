4d1b36e98e3ec573d96a17f4ed8704c0cd84680c
Nepmia <yukana.miasu@gmail.com>
2021-11-19 16:07:14 +0100

    Auto stash before rebase of "origin/main"

--- static/js/InstantNav.js
+++ static/js/InstantNav.js
@@ -31,7 +31,7 @@ $(document).on("click", ".pageChanger", function(){
     let newUrl = encodeURI($(this).attr("page"));
     console.log(newUrl);
     let newTitle = $(this).attr("title") + " - StreetZine";
-    changePage(newUrl, newTitle)
+    changePage(newUrl, newTitle, "yes")
     window.history.pushState(newUrl, url_data.previous_title, url_data.current_page);
 })
 
