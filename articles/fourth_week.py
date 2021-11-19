cc1fe40b28e88ac3cce2df99d5518edfb605e6ea
DrawLou <74010593+DrawLou@users.noreply.github.com>
2021-11-19 11:52:00 +0100

    add article fourth week

--- articles/fourth_week.py
+++ articles/fourth_week.py
@@ -3,38 +3,53 @@ fourth_week = {
     "article":[       
         {
            "main":1,
-           "title":"",
-           "author":"",
-           "date":"",
-           "content":{}
+           "title":"Street Art en Pologne, dans le quartier Zaspa à Gdansk",
+           "author":"Callista",
+           "date":"03/11/2021",
+           "content":{
+               "text" : "Dans l’univers du street art, la Pologne est connue pour posséder un quartier qui est une des plus grandes galeries à ciel ouvert. Dans le quartier résidentiel de Zaspa, à Gdansk, on y retrouve plus de 54 œuvres murales, de quoi faire de la ville une capitale du street art, avec une des plus grandes collections de fresques. <br> C’est en 1979 qu’a eu lieu la révolution artistique en Pologne, à l’occasion d’un festival pour le 1000ème anniversaire de Gdansk. C’est aussi à ce moment-là que la toute première peinture murale a été créée, ainsi que neuf autres pièces. En 2009, le Festival a été relancé, ajoutant désormais 4 œuvres chaque année. C’est ainsi qu’ils égayent les rues et amènent une cohésion sociale sur le plan local. <br> Parmi les artistes polonais, on peut citer Mariusz Waras, qui est considéré comme l’un des leaders mondiaux du street art, Przemek Blejzyk aka Sainer et Mateusz Gapski aka Bezt, qui sont les fondateurs du team Etam Cru, Michał Wręga, Natalia Rak ou encore Elżbieta Dymn. Cette dernière utilise par exemple la dentelle traditionnelle à une échelle immense puisqu’elle l’applique à l’espace urbain. <br> Si Gdansk est une des villes reconnues pour être une capitale du street art, des associations cherchent à redonner vie à des villes qui paraissent au premier abord moins accueillantes. C’est le cas de Lodz, une ville dans laquelle ils ont décidé de lui donner une identité unique avec des programmes de rénovation urbaine pour la faire entrer dans une toute nouvelle dimension touristique et économique. <br> N’hésitez donc pas à aller visiter ces quartiers si jamais vous décidez de voyager en Pologne. "
+               "image1" : ""
+           }
        },
         {
            "main":0,
-           "title":"",
-           "author":"",
-           "date":"",
-           "content":{}
+           "title":"Street Art à Londres",
+           "author":"Louise",
+           "date":"01/11/2021",
+           "content":{
+               "text" : "Week-end à Londres ? Alors ne manquez pas de découvrir le Street Art dans la capitale du Royaume-Uni, incontournable dans votre séjour à Londres ! <br> La capitale du Royaume-Uni est une ville multiculturelle, ouverte sur le monde et artistiquement dynamique. Personne ne sera alors étonné qu’elle regorge d'œuvres, de fresques et de graffitis. Et le Street Art à Londres est depuis longtemps devenu un incontournable à visiter, notamment grâce à des artistes comme Banksy, Stik ou D*Face. La scène londonienne est en constante évolution, et les œuvres peuvent vite disparaître ou être remplacées. Si vous aimez le Street Art, vous ne serez pas déçus. La ville ne cesse de se renouveler et vous ne manquerez pas d'œuvres à découvrir ! <br> Le Street Art à Londres se retrouve principalement dans cinq quartiers : Bricklane, Shoreditch, Camden Town, Brixton et Hackney Wick. Ces derniers sont considérés comme les meilleurs pour découvrir l’art urbain de la ville. L’est de Londres et le quartier Shoreditch sont particulièrement connus pour abriter des œuvres de Street Art réalisées par des artistes du monde entier. Vous y découvrirez notamment des œuvres des très célèbres Banksy, ROA, Invader ou Shepherd Fairy. <br> Un autre endroit iconique du Street Art à Londres est le Banksy Tunnel, également connu sous le nom de Leake Street Tunnel, ce lieu a été rendu célèbre par Banksy. Il se situe dans un tunnel sous la gare de Waterloo. Les œuvres sont très éphémères et si vous y passez un jour, vous apercevrez même des artistes en train de travailler. <br> Durant votre séjour, ne vous contentez pas de visiter les quartiers évoqués. Le Street Art à Londres ne s’apprécie pas uniquement en admirant des fresques murales dans la rue. De nombreuses organisations proposent également, outre des visites guidées, des initiations à l’art urbain. Vous pourrez alors peindre vos propres œuvres et vous essayer au Street Art dans un studio aménagé. Les plus talentueux.ses pourront aussi laisser leurs marques sur les murs de la ville ! "
+               "image1" : ""
+           }
        },
         {
            "main":0,
-           "title":"",
-           "author":"",
-           "date":"",
-           "content":{}
+           "title":"Street Art à Melbourne",
+           "author":"Louise",
+           "date":"02/11/2021",
+           "content":{
+               "text" : "Si vous prévoyez de voyager en Australie, alors n’hésitez pas à passer à Melbourne, Capitale du Street Art en Australie, découvrez son street art et ses nombreux tags sera un arrêt incontournable ! <br> Melbourne offre de nombreux endroits en plein centre où le street art est à l’honneur et approuvé par la ville elle-même. Le street art à Melbourne est en effet très développé et fait partie des lieux où les touristes n’hésitent pas à venir prendre quelques photos. <br> En découvrant cette ville, les amateurs de Street Art ne seront pas déçus ! Melbourne est incontestablement la ville la plus artistique d’Australie, et l’art de rue est largement représenté. Dès les années 1970 et 80 de nombreux artistes de Street Art ont été influencés par les graffs New Yorkais, s’exprimant dans les rues de la ville et le long des lignes de train. Mais c’est au début des années 2000 que cet art s’est véritablement développé sous de multiples formes, telles que le Sticker art, poster art, wood blocking etc… Aujourd’hui de nombreux artistes internationaux viennent marquer leur passage dans les rues de Melbourne ou dans les galeries. <br> Si vous voulez découvrir de belles fresques en plein cœur de la ville, rendez-vous sur Union Lane et Hosier Lane. En face de Federation Square et rejoignant Flinders Lane avec Flinders Street, la rue pavée Hosier Lane est sans doute le point central du street art de la ville. <br> Le quartier de Fitzroy laisse lui aussi place à de nombreux graffs. Entre 2 fripes, n’hésitez pas à passer un peu de temps dans les petites ruelles ! <br> Pour les fans de musique, AC/DC Lane a été nommé en 2004 pour rendre hommage au groupe de rock australien. Comme vous vous en doutez, le street art présente toutes sortes de musiciens, réels et imaginaires. <br> D’autres quartiers, tels que Collingwood, Prahran ou Footscray par exemple, ont eux aussi quelques rues passées à la bombe. À explorer ! <br> Pensez également à faire un tour au cœur de Chinatown et admirer les superbes tags sur Croft Alley."
+               "image1" :""
+           }
        },
         {
            "main":0,
-           "title":"",
-           "author":"",
-           "date":"",
-           "content":{}
+           "title":"Musée du Street Art à Berlin",
+           "author":"Clément",
+           "date":"04/11/2021",
+           "content":{
+               "text" : "Vandalisme, pollution visuelle ou chef-d'œuvre ? Sans surprise, la capitale allemande riche en fresques murales a tranché. Elle offre des murs au street art avec son nouvel espace Urban Nation. Ce musée, qui est le plus grand au monde à se consacrer aux graffitis de manière permanente, est le premier musée consacré au Street Art à Berlin. Ce musée, qui a ouvert ses portes en septembre 2017, offre une scénographie astucieuse et une exposition de près de 150 œuvres d'une centaine d’artistes de street art, dont celles du célèbre Banksy et de Shepard Fairey. Les œuvres, collectées depuis trois ans par la fondation Urban Nation, seront visibles aussi bien à l’extérieur qu’à l’intérieur du musée. La plupart sont reproduites sur toile et non sur pierres ou briques pour mieux les conserver. <br> Urban Nation propose une bourse de 6 mois à Berlin nommée Fresh A.I.R pour treize artistes ou professionnels de la culture dans le domaine de l'Art Urbain et Nouvel Art Contemporain. Il propose aussi des projets pour le divertissement et l’éveil artistique des personnes y participant. Ces projets sont “ONE WALLS”, “PROJEKTRAUM“, “COMMUNITY WALLS” et “PROJEKT M/”"
+               "image1" : ""
+           }
        },
         {
            "main":0,
-           "title":"",
-           "author":"",
-           "date":"",
-           "content":{}
+           "title":"Street Art au Japon",
+           "author":"Louise",
+           "date":"05/11/2021",
+           "content":{
+               "text" : "L’art urbain japonais est un courant artistique qui passionne énormément de gens. Il s’est développé dans les rues vers la fin du 19ème siècle. Ce dernier se caractérise par des formes géométriques et des couleurs vives faisant des fresques murales japonaises les plus belles du monde. <br> Notons que le Street Art ne faisait pas partie de la culture japonaise. Aujourd’hui au Japon, le Street Art est considéré comme un acte de vandalisme ou de sabotage des places publiques qui pullulent les villes. Néanmoins, jusqu’à nos jours, les artistes n’hésitent pas à prendre de gros risques pour laisser s’exprimer leurs talents sur les murs nippons. En effet, cette pratique est sévèrement punie par la loi japonaise. Les artistes peuvent risquer de 21j à 5 ans de prison. <br> Malgré que la pratique du Street art sur les murs japonais est hautement sanctionnée par la loi, certains artistes n’ont pas manqué de se hisser au rang des meilleurs artistes de rue. Cela s’explique par les résultats de leurs performances dans cette pratique. D’autres sont parvenus à avoir une renommée internationale et se sont fait connaître à travers le monde entier. <br> Tout de même le Street Art est pratiqué dans plusieurs villes du Japon. Certaines villes regorgent de graffitis des Streets Artistes, il s’agit essentiellement de la ville de Kobe, Osaka et Tokyo. <br>Avez-vous déjà entendu parler des manhôru ? Ou bien encore appelé “l’art des plaques d’égout”, le manhôru est apparu dans les années 1980 au Japon. Le but étant de sensibiliser la population à faire relier leur maison au système d’assainissement collectif, les plaques d’égout ont commencé anisi à être personnalisées, dans l’optique de les rendre plus attrayants et à exciter la population à s’en procurer. <br> On retrouve aujourd’hui dans les rues japonaises environ 13 000 différentes sortes de plaque d’égout. Aujourd’hui, elles font l’objet de collection d’art qui est souvent vendu aux enchères. De même, notons que très souvent, chaque représentation sur les plaques d’égout a sa signification. C’est le cas de certaines villes Japonaises qui sont faciles à reconnaitre juste en observant le symbole que portent les plaques d’égouts de cette ville. En effet, ces symboles sont souvent liés à un fait historique ayant marqué cette ville."
+               "image1" : ""
+           }
        },
      ]
 }
