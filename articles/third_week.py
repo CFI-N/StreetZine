287b716ef12867ca765f76b586da7ebc64fb0c0b
Mastaudonte <74010157+Mastaudonte@users.noreply.github.com>
2021-11-19 10:20:11 +0100

    add date and text in third_week

--- articles/third_week.py
+++ articles/third_week.py
@@ -5,52 +5,52 @@ third_week = {
            "main":0,
            "title":"Parcours du street art dans Paris",
            "author":"Clément",
-           "date":"",
+           "date":"26/10/2021",
            "content":{
-               "text":"",
-               "image1":"",
+               "text":"Le street art envahit la plupart des plus grandes villes du Monde, n’a pas fait exception de la grande ville qu’est Paris. La question que vous vous posez sans doute est,<br><strong>Quel parcours devrais je emprunter pour voir un maximum d'œuvres<br>dans Paris ?</strong><br><br>Pour répondre à cette question, nous vous proposons d'aller consulter le site, LA VOIX DE L’ART URBAIN. Vous y trouverez une carte répertoriant les différentes œuvres avec leurs photos et leurs positions dans Paris. Ce qui vous permettra de vous faire votre propre parcours.<br><br>Même si certains parcours existent déjà, comme la chasse aux space invaders qui se situe dans le 13ème arrondissement.<br>Sinon vous pouvez partir en promenade dans Paris et chercher par vous même ces œuvres incontournables, nous vous donnons quand même un conseil, cherchez plus du côté du 3ème, du 13ème et du 20ème car ce sont les arrondissements où il y a le plus de ces œuvres.<br>N’oubliez pas qu’il existe une galerie d’art à Paris vous permettant de voir du street art simplement, il s’agit de la galerie d’art du 59 rue rivoli.",
+               "image1":"https://www.pullseo.com/wp-content/uploads/2013/08/space-invader-paris.jpg",
            }
        },
        {
            "main":0,
            "title":"Parcours du street art dans Strasbourg ",
            "author":"Apolline",
-           "date":"",
+           "date":"27/10/2021",
            "content":{
-               "text":"",
-               "image1":"",
+               "text":"Chers lecteurs, chères lectrices, si vous avez l’opportunité d’aller à Strasbourg, l’une des capitales de l’Union Européenne, vous y découvrirez 3 parcours de street art avec une grande variété de technique.<br>Que ce soit à Gare, Krutenau ou Schiltigheim, vous pourriez admirer une exposition d’art à ciel ouvert.<br>Parmi les incontournables se trouve, Rue du Général Conrad, l'œuvre de Felix Apaiz Wysocki faite en 2020, une fresque en graffiti intitulée « Retour à l’anormal » de 50m x 4m.<br><br>Durant l’année 2020 est apparue, Rue de la Vignette, « la tête dans les nuages , une fresque de Dan 23 qui fut un challenge créatif par CultureAngels (composé de l'association Soundsitiv et du fonds de dotation éponyme CultureAngels) et l'Association de la Rue de la Vignette de Strasbourg. Ce défi a pour but de récolter des dons afin de soutenir la défense des droits des enfants et des jeunes. Le challenge est de recouvrir entièrement une place de Strasbourg avec une œuvre de 1000 m2.<br><br>Dans le parc de Pourtalès, situé au nord-est du quartier de la Robertsau, c'est l'un des plus grands parcs de la ville. En avril 2018, Claudio Parmiggiani crée une installation IL BISCO GUARDA E ASCOLTA « la forêt regarde et écoute ». Cette œuvre combine la nature et le bronze, de grandes oreilles en bronze associées aux arbres qui peuvent laisser apparaître des yeux (les traces laissées par la coupe opérée sur des branches proches de la racine).<br><br>RUE DU FOSSÉ DES TANNEURS se trouve le Pochoir représentant "Käpt´n Kalle" réalisé en Avril 2019.<br>La boîte aux lettres a elle été réalisée par l'artiste peintre ZOUZ D.<br><br>Il y a encore de nombreuses œuvres à contempler dans la ville des marchés de Noël.",
+               "image1":"http://streetartparis.fr/wp-content/uploads/2015/04/IMG_2188-Zoo-Project.jpg",
            }
        },
        {
            "main":0,
            "title":"Festival Peinture Fraîche à Lyon",
            "author":"Callista",
-           "date":"",
+           "date":"28/10/2021",
            "content":{
-               "text":"« La ville de Lyon s’est positionnée très tôt en tant que précurseur à travers les commandes des célèbres murs peints des Lyonnais à la Croix-Rousse ou en Presqu’île. » Aujourd’hui, la ville de Lyon a permis de démocratiser le street art à travers de nombreux événements comme le Festival Peinture Fraîche à la Halle Debourg. Rassemblant plus de 50 street artistes, il a été organisé du 01 Octobre jusqu’au 7 novembre 2021, il a été allongé pendant les vacances scolaires pour permettre aux enfants de venir et de participer. En effet, ils se proclament « Kidz Friendly » et on le voit notamment au niveau des ateliers organisés : durant tout le festival, des ateliers ponctuels sont organisés comme le graffiti miniature sur maquette, la customisation d’une bombe de peinture, le croquis de graffiti, les tattoo éphémères, le lettrage pour écrire son alphabet en graffiti, ainsi qu’une sensibilisation aux NFT’s. <br>Les NFT’s, c’est quoi ? NFT signifie « non fungible tokens », soit Jeton non fongible. Il s’agit d’un type spécial de jeton cryptographique.<br>« LES NFT’s sont des jetons numériques uniques, non reproductibles, qui permettent d'authentifier un objet réel ou virtuel. Basés sur la technologie de la blockchain, ils sont notamment utilisés pour identifier des œuvres d'art numérique qui peuvent ainsi être achetées, vendues ou échangées. »<br><br>Ils organisent aussi des ateliers permanents : des coloriages à réalité augmentée, un jeu participatif, des bornes d’arcade, des graffitis sur Ipad, des sérigraphies en direct, des murs d’expression libre ainsi que des murs de graffiti. Ils organisent aussi des live painting lors desquels l’un des 50 artistes invités viendra faire une œuvre devant vos yeux. Ils proposent même des visites guidées. Ils ont cherché à faire transparaître de nombreux mouvements qui appartiennent au street art qui font du graffiti au post graffiti en passant par l’artivisme, l’onirisme, l’hyper réalisme ou encore l’abstraction.<br>Cette année, ils ont même vu quatre tendances fortes qui se sont dégagées : les nouvelles technologies, l’écologie, les regards féminins et l’abstraction. En y associant la présence des enfants, ils parviennent ainsi à sensibiliser un maximum de personnes et à faire passer des messages forts qui marqueront sûrement ces personnes à vie.v",
+               "text":"« La ville de Lyon s’est positionnée très tôt en tant que précurseur à travers les commandes des célèbres murs peints des Lyonnais à la Croix-Rousse ou en Presqu’île. » Aujourd’hui, la ville de Lyon a permis de démocratiser le street art à travers de nombreux événements comme le Festival Peinture Fraîche à la Halle Debourg. Rassemblant plus de 50 street artistes, il a été organisé du 01 Octobre jusqu’au 7 novembre 2021, il a été allongé pendant les vacances scolaires pour permettre aux enfants de venir et de participer. En effet, ils se proclament « Kidz Friendly » et on le voit notamment au niveau des ateliers organisés : durant tout le festival, des ateliers ponctuels sont organisés comme le graffiti miniature sur maquette, la customisation d’une bombe de peinture, le croquis de graffiti, les tattoo éphémères, le lettrage pour écrire son alphabet en graffiti, ainsi qu’une sensibilisation aux NFT’s.<br>Les NFT’s, c’est quoi ? NFT signifie « non fungible tokens », soit Jeton non fongible. Il s’agit d’un type spécial de jeton cryptographique.<br>« LES NFT’s sont des jetons numériques uniques, non reproductibles, qui permettent d'authentifier un objet réel ou virtuel. Basés sur la technologie de la blockchain, ils sont notamment utilisés pour identifier des œuvres d'art numérique qui peuvent ainsi être achetées, vendues ou échangées. »<br><br>Ils organisent aussi des ateliers permanents : des coloriages à réalité augmentée, un jeu participatif, des bornes d’arcade, des graffitis sur Ipad, des sérigraphies en direct, des murs d’expression libre ainsi que des murs de graffiti. Ils organisent aussi des live painting lors desquels l’un des 50 artistes invités viendra faire une œuvre devant vos yeux. Ils proposent même des visites guidées. Ils ont cherché à faire transparaître de nombreux mouvements qui appartiennent au street art qui font du graffiti au post graffiti en passant par l’artivisme, l’onirisme, l’hyper réalisme ou encore l’abstraction.<br>Cette année, ils ont même vu quatre tendances fortes qui se sont dégagées : les nouvelles technologies, l’écologie, les regards féminins et l’abstraction. En y associant la présence des enfants, ils parviennent ainsi à sensibiliser un maximum de personnes et à faire passer des messages forts qui marqueront sûrement ces personnes à vie.",
                "image1":"https://lh6.googleusercontent.com/3n7siKXpoCmPOlkdIltkUrhlkMBoicRGSn8oold8Z9oqbkcTU36HThMXpx2ssXowEVfNICm800HePmEvXBTFm9jWulrMevlbGAyjYj339lTa6oX_waFYDxqKhQaQDe51E-z5Xfv8",
            }
        },
        {
-           "main":0,
+           "main":1,
            "title":"Musée du street art, Street Art City à Lurcy-Lévis",
-           "author":"Stephen",
-           "date":"",
+           "author":"Clément",
+           "date":"29/10/2021",
            "content":{
-               "text":"",
-               "image1":"",
+               "text":"Le musée du street art de Lurcy-Lévis est un musée qui ne cloisonne pas l’art qu’est le street art, car il ne se limite pas à quatre murs, il se démarque en proposant à des artistes du monde entier l’accès à une ville entière nommée Street Art City pour exposer leurs œuvres aux yeux de tous, avec plusieurs scènes artistiques, dont 5 espaces d’exposition et un hôtel nommé “hôtel 128” en guise de showroom.<br>Street Art City se compose d’un espace de 10 hectares avec 22 500 m² de mur et de façade en perpétuelle évolution, et de 7 000 m² de construction avec 13 bâtiments. Tout cet espace permet aux artistes de s'exprimer librement, ce qui se remarque par la quantité des œuvres. Il y a environs 90 fresques extérieures, une multitude d’oeuvres interne aux différents bâtiments et 94 oeuvres-cellules pour un total de 300 artistes avec près de 60 nationalités<br>Aujourd’hui désaffecté, l'hôtel 128 est entièrement livré aux artistes du monde entier selon un cahier des charges. Ainsi, chaque année de mars à août, les artistes interviennent en investissant une chambre et en intégrant leurs toiles sur les murs. Dès le 1er Septembre, le nombre de chambres réalisées au cours du printemps-été précédent est ouvert aux visiteurs.<br>L’expérience unique de l'hôtel 128 donne l’occasion d’être immergé au cœur de différentes œuvres sur près de 4 étages avec 128 chambres, ce qui permet d’en ressentir au maximum les émotions, la fantaisie, l’histoire et l’ambiance des œuvres.",
+               "image1":"https://france3-regions.francetvinfo.fr/image/NRkUe0V1s8heHK2vrgm5F4i_ots/1200x675/regions/2020/06/28/5ef8a33fc8b5b_img_1270-4899554.jpg",
            }
        },
        {
            "main":0,
            "title":"Street art à Bordeau",
            "author":"Louise",
-           "date":"",
+           "date":"30/10/2021",
            "content":{
                "text":"",
-               "image1":"",
+               "image1":"https://mlfrndqtah2j.i.optimole.com/7FB402E-q6k8Ml4X/w:1919/h:1080/q:auto/https://lesparesseuxcurieux.fr/wp-content/uploads/2020/10/DSCF5996.jpg",
            }
        },
     ],
-    "number":1,
+    "number":5,
 }
