91cb2588ce25b1bf26b43be00e31c314c7527b12
Nepmia <yukana.miasu@gmail.com>
2021-11-25 05:41:02 +0100

    Added footcalc functions, disabled for now cause it makes the footer disapear for unknow reasons

---
+++ static/js/footCalcs.js
@@ -0,0 +1,17 @@
+// function footerCalc() {
+//     if ($(window).height() < $(document).height()) {
+//         // pass
+//     } else {
+//         let footer = $("footer")
+//         footer.css({
+//             "position" : "absolute",
+//             "top" : $(window).height() - 76
+//         })
+//     }
+//     // console.log($(window).height);
+//     // console.log($(document).height);
+// }
+
+// $(document).ready(() => {
+//     footerCalc();
+// })
